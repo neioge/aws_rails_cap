673c3ebb1412d1f27e6c618dc8e6f1c3d10acf80
neioge <katanonaoto@gmail.com>
2021-01-11 03:28:00 +0000

    add guard

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,10 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+# CLI上のテストの結果を見やすくするためのgem
+require "minitest/reporters"
+Minitest::Reporters.use!
+
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
