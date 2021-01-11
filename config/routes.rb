9a3555988678bcffab1956b9f7212d001acb4725
neioge <katanonaoto@gmail.com>
2021-01-11 03:16:22 +0000

    finish static-pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'   
   get  'static_pages/home'
-  get  'static_pages/help'
-  root 'application#hello' 
+  get  'static_pages/about_me'
+  get  'static_pages/about_app'
 end
